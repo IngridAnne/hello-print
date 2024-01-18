501eff78d18e97c3a04da80c61741554bc6e996c
IngridAnne <ingridanne3009@gmail.com>
2024-01-18 09:42:10 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 09:38:53 2024
+
+@author: ingridanneomtveitlundem
+"""
+
+
+print("Hello Sofia!! Hvordan har dagen vært?")
